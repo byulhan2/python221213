e4ce8341ccbda14b7d52133a6bee8783e4dea7fb
byulhan2 <egodz7@naver.com>
2022-12-14 09:27:17 +0900

    클래스 연습1

---
+++ class1.py
@@ -0,0 +1,17 @@
+# class1.py
+#클래스 정의
+
+class person:
+        #초기화 메서드
+        def __init__(self):
+            self.name="default name"
+
+        #인스턴스 메서드
+        def print(self):
+            print("my name is {0}".format(self.name))
+
+
+#인스턴스 생성
+p1 = person()
+#메서드 호출
+p1.print()
