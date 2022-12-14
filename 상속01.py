95885cc64f438f85377e82787493f8f9eaea38a1
byulhan2 <egodz7@naver.com>
2022-12-14 11:56:52 +0900

    클래스 상속연습2

--- 상속01.py
+++ 상속01.py
@@ -8,10 +8,16 @@ class Person:
 
 class Student(Person):
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #명시적으로 부모의 초기화 메서드 호출
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
+    #상속받은 메서드를 재정의
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(
+            self.name, self.phoneNumber))
+        print("Info(Name:{0}, Phone Number: {1})".format(
+            self.subject, self.studentID))
 
 
 p = Person("전우치", "010-222-1234")
@@ -19,3 +25,5 @@ s = Student("이순신", "010-111-1234", "컴공", "991122")
 p.printInfo()
 s.printInfo()
 
+# print(p.__dict__)
+# print(s.__dict__)
